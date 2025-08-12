b73ffc839cae80bfac586665fd2470bf574412d1
ratnam <bharathiinstitute1@gmail.com>
2025-08-12 18:18:58 +0530

    jhhj

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+// Add this import for TeX support
+import 'package:flutter_math_fork/flutter_math.dart';
+
 ///hello
 void main() {
   runApp(const MainApp());
@@ -12,7 +15,18 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          // Display both plain text and TeX
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hello World!'),
+              SizedBox(height: 20),
+              Math.tex(
+                r'\int_{a}^{b} x^2 dx',
+                textStyle: TextStyle(fontSize: 24),
+              ),
+            ],
+          ),
         ),
       ),
     );
